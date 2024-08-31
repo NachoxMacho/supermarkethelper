59cc28f31e843f95271ec91d8c1307a77a8e8813
Rob Owens <rtowens17@gmail.com>
2024-08-30 21:01:45 -0500

    remove plugins from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
  	content: [ "./**/*.html", "./**/*.templ", "./**/*.go", ],
 	safelist: [],
 	plugins: [
-        require("daisyui"),
-        require('@tailwindcss/forms'),
     ],
-	daisyui: {
-		themes: ["sunset"]
-	}
 }
 
 
